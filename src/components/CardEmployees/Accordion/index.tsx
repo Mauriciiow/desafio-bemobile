2dfeed44fb12716c28b5e5f8e8b3711625e60d1e
Mauricio Oliveira <mauricio@MacBook-Air-de-Mauricio.local>
2023-11-10 11:16:32 -0300

    create: accordion for table

---
+++ src/components/CardEmployees/Accordion/index.tsx
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+import {AccordionProps} from './@types';
+import {
+  EmployeeName,
+  ContaineCard,
+  EmployeePhoto,
+  ChevronDownIcon,
+  ViewContent,
+  ChevronDownButton,
+  OpenCardView,
+  OpenCardText,
+} from './styles';
+import {IconProvider} from '../../../providers/IconProvider';
+import {Animated, LayoutAnimation} from 'react-native';
+import {animation} from './utils';
+
+export const Accordion = ({data}: AccordionProps) => {
+  const [showContent, setShowcontent] = useState<boolean>(false);
+
+  const toggleAnimation = () => {
+    LayoutAnimation.configureNext(animation);
+    setShowcontent(!showContent);
+  };
+
+  return (
+    <ContaineCard>
+      <ViewContent>
+        <EmployeePhoto source={{uri: data.image}} />
+        <EmployeeName>{data.name}</EmployeeName>
+        <ChevronDownButton onPress={() => toggleAnimation()}>
+          <ChevronDownIcon source={IconProvider.chevron_down} />
+        </ChevronDownButton>
+      </ViewContent>
+      {showContent && (
+        // eslint-disable-next-line react-native/no-inline-styles
+        <Animated.View style={{marginTop: 8, overflow: 'hidden'}}>
+          <OpenCardView>
+            <OpenCardText location="left">Cargo</OpenCardText>
+            <OpenCardText location="right">{data.job}</OpenCardText>
+          </OpenCardView>
+          <OpenCardView>
+            <OpenCardText location="left">Data de admissão</OpenCardText>
+            <OpenCardText location="right">{data.admission_date}</OpenCardText>
+          </OpenCardView>
+          <OpenCardView>
+            <OpenCardText location="left">Telefone</OpenCardText>
+            <OpenCardText location="right">{data.phone}</OpenCardText>
+          </OpenCardView>
+        </Animated.View>
+      )}
+    </ContaineCard>
+  );
+};
---
+++ src/components/CardEmployees/Accordion/styles.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components/native';
+type OpenProps = {
+  location: 'left' | 'right';
+};
+export const ContaineCard = styled.View`
+  background-color: ${({theme}) => theme.colors.white};
+  padding: ${({theme}) => theme.spacing.regular16}px;
+`;
+export const ViewContent = styled.View`
+  align-items: center;
+  flex-direction: row;
+`;
+export const EmployeeName = styled.Text`
+  color: ${({theme}) => theme.colors.black};
+  font-family: ${({theme}) => theme.fonts.h3.family};
+  font-size: ${({theme}) => theme.fonts.h3.size}px;
+  text-align: center;
+  flex: 1;
+`;
+
+export const EmployeePhoto = styled.Image`
+  width: 34px;
+  height: 34px;
+  border-radius: 99px;
+`;
+export const ChevronDownButton = styled.TouchableOpacity``;
+export const ChevronDownIcon = styled.Image``;
+export const OpenCardView = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  border-style: dashed;
+  border-width: 2px;
+  border-color: ${({theme}) => theme.colors.gray10};
+  margin: -2px;
+  margin-bottom: 0;
+`;
+export const OpenCardText = styled.Text<OpenProps>`
+  font-family: ${({theme, location}) =>
+    location === 'left' ? theme.fonts.h2.family : theme.fonts.h3.family};
+  color: ${({theme}) => theme.colors.black};
+  font-size: ${({theme}) => theme.fonts.h2.size}px;
+  margin-top: ${({theme}) => theme.spacing.regular16}px;
+`;
