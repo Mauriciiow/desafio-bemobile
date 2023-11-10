59e9ae09e5bbfc766dda71c5d9af5076b04d9a28
Mauricio Oliveira <mauricio@MacBook-Air-de-Mauricio.local>
2023-11-10 15:53:57 -0300

    feat: shadows

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,9 +1,13 @@
+import {Platform} from 'react-native';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
   background-color: ${({theme}) => theme.colors.black};
   height: 60px;
   justify-content: center;
+  ${Platform.OS === 'ios'
+    ? 'shadow-color: #000; shadow-offset: 0 2px; shadow-opacity: 0.3; shadow-radius: 2px;'
+    : 'elevation: 5;'}
 `;
 
 export const Logo = styled.Image`
