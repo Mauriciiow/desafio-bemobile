0655444c0f59d63f36605e5f0479ff5270c27800
Mauricio Oliveira <mauricio@MacBook-Air-de-Mauricio.local>
2023-11-10 18:14:28 -0300

    chore: update archive

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Home} from './src/Pages/Home';
 import {ThemeProvider} from 'styled-components/native';
-import {Theme} from './src/Style/theme';
+import {Theme} from './src/Styles/theme';
 
 const App = () => {
   return (
