24898241265d97c9a1992a08ee8e7a7cbd1a72d3
Mauricio Oliveira <mauricio@MacBook-Air-de-Mauricio.local>
2023-11-13 09:41:07 -0300

    refactor: update name archives

--- src/components/DashedBorderBottom/index.tsx
+++ src/components/DashedBorderBottom/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import Svg, {Line} from 'react-native-svg';
-import {Theme} from '../../Styles/theme';
+import {Theme} from '../../styles/theme';
 
 export const DashedBorderBottom = ({}) => (
   <View style={styles.container}>
